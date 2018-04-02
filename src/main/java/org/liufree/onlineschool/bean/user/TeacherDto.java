2f9688697cd87bb47f3121298f4035496e48001b
liufree <liufreeo@gmail.com>
2018-04-02 13:31:59 +0800

    1

---
+++ src/main/java/org/liufree/onlineschool/bean/user/TeacherDto.java
@@ -0,0 +1,17 @@
+package org.liufree.onlineschool.bean.user;
+
+import lombok.Data;
+
+/**
+ * @author lwx
+ * @date 3/30/18
+ * @email liufreeo@gmail.com
+ */
+@Data
+public class TeacherDto {
+    int id;
+
+    String username;
+
+    String courses;
+}
