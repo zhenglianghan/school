f2b853af5bc69b776876ab73364efb06a38418f8
CoCo-Flyer <694945838@qq.com>
2018-10-09 15:36:19 +0800

    add timestamp

--- src/main/java/org/liufree/onlineschool/dao/user/UserDao.java
+++ src/main/java/org/liufree/onlineschool/dao/user/UserDao.java
@@ -3,9 +3,14 @@ package org.liufree.onlineschool.dao.user;
 import org.liufree.onlineschool.bean.course.Course;
 import org.liufree.onlineschool.bean.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.sql.Timestamp;
+
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
@@ -26,7 +31,13 @@ public interface UserDao extends JpaRepository<User, Integer> {
     @Query("select u from User u where role=:role")
     List<User> getListByRole(@Param("role") int role);
 
+    @Query("select u from User u where u.email=:email")
+    User getUserList(@Param("email") String  email);
 
+    @Transactional
+    @Query(nativeQuery = true,value="insert into log(userName,role,loginTime,logoutTime) values(?1,?2,?3,?4) ")
+    @Modifying
+    public void loginTime(String userName,int role,Timestamp loginTime,Timestamp logoutTime);
 
 
     User findTopByEmailAndPassword(String email, String password);
