7c0708aa9de8035905135100677e721b072540a1
zhenglianghan <736557856@qq.com>
2018-10-24 15:45:03 +0800

    单元的耗时现在可以设置为小数

--- src/main/resources/static/adminlte/js/student/files.js
+++ src/main/resources/static/adminlte/js/student/files.js
@@ -2,16 +2,19 @@
  * Created by hama on 2018/10/24.
  */
 $(function(){
+    var table = $("table")[0];
     //完成状态
     (function(){
-        var table = $("table")[0],
-            totalTimeMin,
+        var totalTimeMin,
             spendTimeMin,
             i,
             len,
-            statement;
+            statement,
+            totalTime;
         for(i=1,len=table.rows.length;i<len;i++){
-            totalTimeMin = parseInt(table.rows[i].cells[1].innerText.slice(0,-1));
+            totalTime = table.rows[i].cells[1];
+            totalTimeMin = Math.floor((parseFloat(totalTime.innerText)*60));
+            totalTime.innerText = totalTimeMin+"m";
             spendTimeMin = parseInt(table.rows[i].cells[2].innerText.split("m")[0]);
             statement = table.rows[i].cells[3];
             if(spendTimeMin>=totalTimeMin){
@@ -22,4 +25,6 @@ $(function(){
             }
         }
     })();
+
+
 })
